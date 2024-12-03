96f8c5f481190d7d8115f87de8cd072aa5c5bb0b
Voon Wong <voon.wong@smartbear.com>
2024-12-03 17:29:39 +1100

    feat: demonstrate how to add dynamic activities to the activity stack

--- packages/marko-run-example/src/routes/docs/session/activity-stack/interstitial.$step+handler.ts
+++ packages/marko-run-example/src/routes/docs/session/activity-stack/interstitial.$step+handler.ts
@@ -2,9 +2,17 @@ import { redirect } from "#lib/responses";
 import type { MySession } from "./types";
 
 export const POST: MarkoRun.Handler = (context) => {
-  (context.session as unknown as MySession)[
-    `interstitial-${Number(context.params.step)}`
-  ] = true;
+  const step = Number(context.params.step);
+
+  if (step <= 3) {
+    (context.session as unknown as MySession)[`interstitial-${step}`] = true;
+  }
+
+  if (step === 2) {
+    // add a bonus interstitial after this step
+    context.activityStack.pop();
+    context.activityStack.push("/docs/session/activity-stack/interstitial/100");
+  }
 
   return context.activityStack.pop() || redirect(context.url.pathname);
 };
