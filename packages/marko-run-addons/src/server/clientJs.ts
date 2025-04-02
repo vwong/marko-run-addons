4b9968bfd91724155c9584f1d9add635f54cde18
Voon Wong <voon.wong@smartbear.com>
2025-04-03 10:26:34 +1100

    fix: don't clobber set-cookie header

--- packages/marko-run-addons/src/server/clientJs.ts
+++ packages/marko-run-addons/src/server/clientJs.ts
@@ -9,7 +9,7 @@ export const clientJs = (): MarkoRun.Handler => async (context, next) => {
   const response = await next();
 
   if (response.headers.get("content-type")?.startsWith("text/html")) {
-    response.headers.set(
+    response.headers.append(
       "Set-Cookie",
       "clientJs=false; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT",
     );
